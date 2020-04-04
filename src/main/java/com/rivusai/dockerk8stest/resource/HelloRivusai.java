e56d1ce01d5dd8e2f9af3dce765806e1a105e237
shlomigonen <shlomi.gonen@gmail.com>
2020-04-03 17:35:16 -0700

    Now integrated with Kubernetes as well

--- src/main/java/com/rivusai/dockerk8stest/resource/HelloRivusai.java
+++ src/main/java/com/rivusai/dockerk8stest/resource/HelloRivusai.java
@@ -11,7 +11,6 @@ public class HelloRivusai {
     @GetMapping
     public String hello () {
         return "Hello from Rivus.ai - your Streaming AI Platform \n" +
-                "(1) Orchestrating with Jenkins using Git, Maven and Docker using Pipeline" +
-                "(2) Testing";
+                "Now Orchestrating with Jenkins Pipeline and integrating with (1) Git (2) Maven (3) Docker (4) Kubernetes";
     }
 }
